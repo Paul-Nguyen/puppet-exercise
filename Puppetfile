be3b7d7e812bf260cb3a280db45cc1d2d39ee368
Paul Nguyen <Paul.Nguyen.Aus@gmail.com>
2015-05-31 06:42:33 +1000

    initial exercise, set up on centos7

---
+++ Puppetfile
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+#^syntax detection
+
+forge "https://forgeapi.puppetlabs.com"
+
+mod 'crayfishx-firewalld', '1.0.0'
+mod 'jfryman-nginx', '0.2.6'
+mod 'jfryman-selinux'
+mod 'puppetlabs-apt'
+mod 'puppetlabs-concat'
+mod 'puppetlabs-git'
+mod 'puppetlabs-stdlib'
+mod 'puppetlabs-vcsrepo'
---
+++ go.pp
@@ -0,0 +1,41 @@
+$webpage_root  = '/usr/share/nginx/html/puppet-exercise'
+$exercise_port = 8000
+$exercise_src  = 'https://github.com/puppetlabs/exercise-webpage.git'
+
+class { 'nginx': }
+class { 'git': }
+
+#selinux::port { 'nginx-port':
+#    port     => $exercise_port,
+#    context  => 'http_port_t',
+#    protocol => 'tcp',
+#} ->
+if ($selinux) {
+    class { 'selinux':
+        mode   => 'permissive',
+        before => Nginx::Resource::Vhost['puppet-nginxercise'],
+    }
+}
+
+nginx::resource::vhost { 'puppet-nginxercise':
+    ensure      => present,
+    server_name => ['puppet-exercise'],
+    www_root    => $webpage_root,
+    listen_port => $exercise_port,
+} ->
+vcsrepo { $webpage_root:
+    ensure   => present,
+    provider => git,
+    source   => $exercise_src,
+    revision => 'master',
+}
+
+firewalld_rich_rule { 'puppet exercise public port':
+    ensure => present,
+    zone   => 'public',
+    port   => {
+                protocol => tcp,
+                port     => $exercise_port,
+              },
+    action => 'accept',
+}
